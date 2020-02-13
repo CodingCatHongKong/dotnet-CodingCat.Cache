0bdbec621837f67948d23a9fd1b72bfe18ade7ff
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-13 19:16:30 +0800

    implemented IStorage

---
+++ CodingCat.Cache/CodingCat.Cache/Interfaces/IStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingCat.Cache.Interfaces
+{
+    public interface IStorage
+    {
+        IStorage Add(IKeyBuilder key, string item);
+        string Get(IKeyBuilder key);
+        IStorage Delete(IKeyBuilder key);
+    }
+}
