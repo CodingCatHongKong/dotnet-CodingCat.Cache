eb48c369cb5bd51e2502af7ec51ecf505da27ba9
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-13 20:47:35 +0800

    enhance the IStorageManager

--- CodingCat.Cache/CodingCat.Cache/Interfaces/IStorageManager.cs
+++ CodingCat.Cache/CodingCat.Cache/Interfaces/IStorageManager.cs
@@ -5,7 +5,13 @@ namespace CodingCat.Cache.Interfaces
 {
     public interface IStorageManager : IStorage
     {
+        IStorage DefaultStorage { get; }
+        IStorage[] Fallbacks { get; }
+
         FallbackPolicy FallbackPolicy { get; }
+
+        IStorageManager SetDefault(IStorage storage);
+        IStorageManager AddFallback(IStorage storage);
 
         string Get(IKeyBuilder key, FallbackPolicy fallbackPolicy);
         string Get(IKeyBuilder key, Func<string> callback, FallbackPolicy fallbackPolicy);
