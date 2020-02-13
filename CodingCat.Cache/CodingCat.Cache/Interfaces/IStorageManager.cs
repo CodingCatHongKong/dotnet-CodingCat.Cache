18a512c986dd5417d3b1ee441075f5a3a671a018
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-13 20:46:15 +0800

    added the fallback policy enum and the storage manager interface

---
+++ CodingCat.Cache/CodingCat.Cache/Interfaces/IStorageManager.cs
@@ -0,0 +1,13 @@
+﻿using CodingCat.Cache.Enums;
+using System;
+
+namespace CodingCat.Cache.Interfaces
+{
+    public interface IStorageManager : IStorage
+    {
+        FallbackPolicy FallbackPolicy { get; }
+
+        string Get(IKeyBuilder key, FallbackPolicy fallbackPolicy);
+        string Get(IKeyBuilder key, Func<string> callback, FallbackPolicy fallbackPolicy);
+    }
+}
