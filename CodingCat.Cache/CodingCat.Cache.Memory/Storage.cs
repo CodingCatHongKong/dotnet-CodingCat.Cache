78cb610f17b81c377a1a28ac22d426ff631add76
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-13 19:16:47 +0800

    implemented Storage in the memory cache project

---
+++ CodingCat.Cache/CodingCat.Cache.Memory/Storage.cs
@@ -0,0 +1,59 @@
+﻿using CodingCat.Cache.Enums;
+using CodingCat.Cache.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingCat.Cache.Memory
+{
+    public class Storage : IStorage
+    {
+        private MemoryCache memoryCache { get; }
+
+        public TimeSpan Expiry { get; }
+
+        #region Constructor(s)
+        public Storage(TimeSpan expiry)
+        {
+            this.memoryCache = MemoryCache.Default;
+
+            this.Expiry = expiry;
+        }
+        #endregion
+
+        public IStorage Add(IKeyBuilder key, string item)
+        {
+            this.memoryCache.Add(
+                key.ToString(),
+                item,
+                DateTimeOffset.Now.Add(this.Expiry)
+            );
+            return this;
+        }
+
+        public string Get(IKeyBuilder key)
+        {
+            if (this.Exists(key))
+            {
+                return this.memoryCache[key.ToString()]
+                    ?.ToString();
+            }
+
+            return null;
+        }
+
+        public IStorage Delete(IKeyBuilder key)
+        {
+            if (this.Exists(key)) this.memoryCache.Remove(key.ToString());
+            return this;
+        }
+
+        private bool Exists(IKeyBuilder key)
+        {
+            return this.memoryCache.Contains(key.ToString());
+        }
+    }
+}
