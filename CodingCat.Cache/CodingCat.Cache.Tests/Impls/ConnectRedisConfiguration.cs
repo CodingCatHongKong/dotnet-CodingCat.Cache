890b06fdec444be1729ec09bae85968489728ca3
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-20 19:45:56 +0800

    added test cases for the redis retry approach

---
+++ CodingCat.Cache/CodingCat.Cache.Tests/Impls/ConnectRedisConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CodingCat.Cache.Redis.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingCat.Cache.Tests.Impls
+{
+    class ConnectRedisConfiguration : IConnectRedisConfiguration
+    {
+        public TimeSpan TimeoutPerTry { get; set; }
+        public TimeSpan RetryInterval { get; set; }
+        public uint RetryUpTo { get; set; }
+    }
+}
---
+++ CodingCat.Cache/CodingCat.Cache.Tests/TestRedisConnect.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using CodingCat.Cache.Redis.Interfaces;
+using CodingCat.Cache.Redis.Extensions;
+using CodingCat.Cache.Tests.Impls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+
+namespace CodingCat.Cache.Tests
+{
+    [TestClass]
+    public class TestRedisConnect
+    {
+        public static readonly IConnectRedisConfiguration ConnectRedisConfiguration = new ConnectRedisConfiguration()
+        {
+            TimeoutPerTry = TimeSpan.FromSeconds(2),
+            RetryInterval = TimeSpan.FromSeconds(1),
+            RetryUpTo = 3
+        };
+
+        [TestMethod]
+        public void Test_ConnectInvalidServer_WillRetry()
+        {
+            const string USING_SERVER = "127.0.0.1:1234";
+
+            // Arrange
+            uint actual = 0;
+            var expected = ConnectRedisConfiguration.RetryUpTo;
+
+            // Act
+            IConnectionMultiplexerExtensions
+                .CreateRedisConnection(
+                    () =>
+                    {
+                        actual += 1;
+                        return ConnectionMultiplexer.Connect(USING_SERVER);
+                    },
+                    ConnectRedisConfiguration.TimeoutPerTry,
+                    ConnectRedisConfiguration.RetryInterval,
+                    ConnectRedisConfiguration.RetryUpTo
+                );
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ConnectInvalidServer_ReturnNull()
+        {
+            const string USING_SERVER = "127.0.0.1:1234";
+
+            // Arrange
+
+            // Act
+            var actual = USING_SERVER.CreateRedisConnection(
+                ConnectRedisConfiguration
+            );
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void Test_ConnectValidServer_Ok()
+        {
+            // Arrange
+
+            // Act
+            var actual = Constants.REDIS_CONFIG
+                .CreateRedisConnection(ConnectRedisConfiguration);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.IsConnected);
+        }
+    }
+}
