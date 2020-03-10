1c1c9263d97b3bfd21c1f89d8b5312d7d702d4e3
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-10 21:19:11 +0800

    added inject and resolve extensions to the base library project for the key builder and storage manager

---
+++ CodingCat.Cache/CodingCat.Cache/InjectExtensions.cs
@@ -0,0 +1,65 @@
+﻿using CodingCat.Cache.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingCat.Cache
+{
+    public static class InjectExtensions
+    {
+        public static IServiceCollection AddKeyBuilder<T>(
+            this IServiceCollection services,
+            IKeyBuilderConfiguration configuration
+        ) where T : class, IKeyBuilder
+        {
+            return services
+                .AddTransient(provider => configuration)
+                .AddTransient<IKeyBuilder, T>();
+        }
+
+        public static IServiceCollection AddKeyBuilder<T>(
+            this IServiceCollection services
+        ) where T : class, IKeyBuilder
+        {
+            return services
+                .AddTransient<IKeyBuilder, T>()
+                .AddSingleton<Func<IKeyBuilderConfiguration, IKeyBuilder>>(
+                    provider => config =>
+                        ActivatorUtilities.CreateInstance<IKeyBuilder>(
+                            provider,
+                            config
+                        )
+                );
+        }
+
+        public static IServiceCollection AddStorageManager<T>(
+            this IServiceCollection services
+        ) where T : class, IStorageManager
+        {
+            return services
+                .AddTransient<IStorageManager, T>()
+                .AddSingleton<Func<IStorage, IStorageManager>>(
+                    provider => defaultStorage =>
+                    {
+                        var fallbacks = provider
+                            .GetService<IEnumerable<IStorage>>()
+                            .Where(fallback =>
+                                fallback.GetType() != defaultStorage.GetType()
+                            );
+
+                        var manager = ActivatorUtilities
+                            .CreateInstance<IStorageManager>(
+                                provider,
+                                defaultStorage
+                            );
+
+                        foreach (var fallback in fallbacks)
+                            manager.AddFallback(fallback);
+
+                        return manager;
+                    }
+                );
+        }
+    }
+}
---
+++ CodingCat.Cache/CodingCat.Cache/ResolveExtensions.cs
@@ -0,0 +1,41 @@
+﻿using CodingCat.Cache.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CodingCat.Cache
+{
+    public static class ResolveExtensions
+    {
+        public static IKeyBuilder Resolve(this IServiceProvider provider)
+        {
+            return provider.GetService<IKeyBuilder>();
+        }
+
+        public static IKeyBuilder Require(this IServiceProvider provider)
+        {
+            return provider.GetRequiredService<IKeyBuilder>();
+        }
+
+        public static IKeyBuilder Resolve(
+            this IServiceProvider provider,
+            IKeyBuilderConfiguration configuration
+        )
+        {
+            return provider
+                .GetService<Func<IKeyBuilderConfiguration, IKeyBuilder>>()(
+                    configuration
+                );
+        }
+
+        public static IStorageManager Resolve(
+            this IServiceProvider provider,
+            IStorage defaultStorage
+        )
+        {
+            return provider
+                .GetService<Func<IStorage, IStorageManager>>()(
+                    defaultStorage
+                );
+        }
+    }
+}
