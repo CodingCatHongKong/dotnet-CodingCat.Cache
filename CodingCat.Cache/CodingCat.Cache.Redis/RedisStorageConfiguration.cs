a20004ed4f62d799fc04d6a0dc989a22cc87665d
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-11 21:15:50 +0800

    enhanced the constructor of redis storage configuration

--- CodingCat.Cache/CodingCat.Cache.Redis/RedisStorageConfiguration.cs
+++ CodingCat.Cache/CodingCat.Cache.Redis/RedisStorageConfiguration.cs
@@ -7,9 +7,23 @@ namespace CodingCat.Cache.Redis
     public class RedisStorageConfiguration : IRedisStorageConfiguration
     {
         public IConnectionMultiplexer RedisConnection { get; set; }
-        public int DatabaseIndex { get; set; } = -1;
 
+        public int DatabaseIndex { get; set; } = -1;
         public TimeSpan Expiry { get; set; }
+
+        #region Constructor(s)
+        public RedisStorageConfiguration() { }
+
+        public RedisStorageConfiguration(
+            IConnectionMultiplexer redisConnection,
+            RedisStorageConfiguration configuration
+        )
+        {
+            this.RedisConnection = redisConnection;
+            this.DatabaseIndex = configuration.DatabaseIndex;
+            this.Expiry = configuration.Expiry;
+        }
+        #endregion
 
         public IDatabase GetDatabase()
         {
